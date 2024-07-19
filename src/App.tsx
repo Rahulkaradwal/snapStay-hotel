3cc4ec183d9398770e5b03d407ef6c75ff661815
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-07-19 13:24:21 -0400

    Initialize the project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <div>
+        <div className="text-center">
+          <h1 className="text-left text-3xl md:bg-red-50">SnapStay Hotel</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
