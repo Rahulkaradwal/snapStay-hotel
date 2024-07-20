d58fc0cc4866f1da137122aeaa757816c4c9e886
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-07-20 13:21:25 -0400

    feat: add function to determine the postion of the header

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,10 +1,25 @@
 import { Outlet } from "react-router-dom";
 import Header from "../ui/Header";
+import { useEffect, useState } from "react";
 
 const AppLayout = () => {
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+      }
+      if (window.scrollY !== 0) {
+        setIsTopOfPage(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
   return (
     <div>
-      <Header isTopOfPage={true} />
+      <Header isTopOfPage={isTopOfPage} />
       <Outlet />
     </div>
   );
