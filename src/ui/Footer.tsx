2a4e38c2541e2ac0bda23a848f13d509be49898d
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-07-22 18:26:34 -0400

    style: fix footer ui

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -3,20 +3,47 @@ import { FaGithub, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 const Footer = () => {
   const socialIcons =
     "text-2xl text-white hover:text-golden-500 transition duration-500 cursor-pointer hover:scale-125";
+
+  const infoClass = "flex text-white flex-col gap-1";
   return (
-    <section className="flex items-center justify-between bg-zinc-900 p-8 text-golden-800">
+    <section className="flex items-start justify-between bg-zinc-900 p-8 text-golden-800">
       <div className="flex flex-col items-center gap-4 p-4">
-        <img className="w-24" src={"/logo-no-background.svg"} alt="logo" />
+        <img className="w-36" src={"/logo-no-background.svg"} alt="logo" />
         <div className="flex gap-4">
           <FaGithub className={socialIcons} />
           <FaInstagram className={socialIcons} />
           <FaLinkedinIn className={socialIcons} />
         </div>
       </div>
-      <div className="">Services</div>
-      <div className="">About</div>
-      <div className="">Our Projects</div>
-      <div className="">Address</div>
+      <div className={infoClass}>
+        <p className="font-bold text-golden-800">Services</p>
+        <p>Booking</p>
+        <p>Cancellation</p>
+        <p>Meals</p>
+        <p>Wifi</p>
+        <p>Laundry</p>
+        <p>Spa</p>
+        <p>Parking</p>
+      </div>
+      <div className={infoClass}>
+        <p className="font-bold text-golden-800">About</p>
+        <p>Our Team</p>
+        <p>Careers</p>
+        <p>FAQ</p>
+      </div>
+      <div className={infoClass}>
+        <p className="font-bold text-golden-800">Our Projects</p>
+        <p>Our Team</p>
+        <p>Terms & Conditions</p>
+        <p>Privacy Policy</p>
+      </div>
+      <div className={infoClass}>
+        <p className="font-bold text-golden-800">Address</p>
+        <p>Toronto</p>
+        <p>33 Dundas Street, Toronto, ON</p>
+        <p>rahulkaradwal@gmail.com</p>
+        <p>289-890-0869</p>
+      </div>
     </section>
   );
 };
