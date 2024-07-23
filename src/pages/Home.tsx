46299373f1002f8a4856d7233a11edc26e6b128a
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-07-23 16:35:30 -0400

    reafactor: ui components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
-import Carousel from "../ui/Carousel";
+import Carousel from "../components/home/Carousel";
 import room1 from "/rooms/asthetic-room.jpg";
 import room2 from "/rooms/beachview.jpg";
 import room3 from "/rooms/lobby-sofa.jpg";
 import room4 from "/rooms/outdoor.jpg";
 import room5 from "/rooms/mountain.jpg";
-import SpecialPackage from "../ui/SpecialPackage";
+import SpecialPackage from "../components/home/SpecialPackage";
 import Footer from "../ui/Footer";
 
 function Home() {
--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,11 +1,13 @@
 import Cover from "../components/Services/Cover";
 import MenuItems from "../components/Services/MenuItems";
+import Footer from "../ui/Footer";
 
 function Services() {
   return (
     <div>
       <Cover />
       <MenuItems />
+      <Footer />
     </div>
   );
 }
