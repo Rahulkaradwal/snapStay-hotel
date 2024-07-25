feab19dc69f8c2643f0a0bb10d7a5f7d5cfc40b4
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-07-25 18:11:43 -0400

    feat: add singup page

--- src/components/auth/SignupUser.tsx
+++ src/components/auth/SignupUser.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 function SignupUser() {
-  return <div>SignupUser</div>;
+  return (
+    <div className="absolute right-0 flex h-full w-1/3 flex-col items-center justify-center p-10 pt-1">
+      <img className="w-60" src={"/logo-no-background.svg"} alt="logo" />
+      <form className="flex flex-col gap-4 pb-10 pt-20">
+        <input
+          type="text"
+          placeholder="Username"
+          className="bg-ligthDark my-4 w-80 rounded-sm border p-2"
+        ></input>
+        <input
+          type="password"
+          placeholder="Password"
+          className="bg-ligthDark rounded-sm border p-2"
+        ></input>
+        <button className="my-4 rounded-sm bg-golden-500 p-2 text-slate-50 transition-all duration-300 hover:bg-golden-800 hover:text-black">
+          Create Account
+        </button>
+      </form>
+      <Link className="text-md text-slate-50" to="/login">
+        Already have an account?
+      </Link>
+    </div>
+  );
 }
 
 export default SignupUser;
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,5 +1,12 @@
+import SignupUser from "../components/auth/SignupUser";
+
 function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <section className="bg-auth-cover relative h-screen bg-cover">
+      <div className="from-dark absolute inset-0 bg-gradient-to-l to-transparent"></div>
+      <SignupUser />
+    </section>
+  );
 }
 
 export default SignUp;
