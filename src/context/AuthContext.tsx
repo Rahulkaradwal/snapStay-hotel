d20eb1cda8843a574a429ce1d73d08995491edfb
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-07-29 18:37:52 -0400

    fix: check token and expiration time on load

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -14,47 +14,55 @@ interface AuthContextProps extends AuthState {
 const AuthContext = createContext<AuthContextProps | undefined>(undefined);
 
 const isTokenExpired = (expiration: number | null) => {
-  if (!expiration) {
-    return true;
-  }
+  if (!expiration) return true;
   return Number(getCurrentTime()) > expiration;
 };
 
 const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
+  // Set token from local storage or null
   const [token, setToken] = useState<string | null>(
     localStorage.getItem("TOKEN_KEY"),
   );
 
-  const [isAuthenticated, setIsAuthenticated] =
-    useState<boolean>(!isTokenExpired);
+  // Set expiration time from local storage or null
+  const [expirationTime, setExpirationTime] = useState<number | null>(() => {
+    const storedValue = localStorage.getItem("EXPIRATION_TIME");
+    return storedValue ? parseInt(storedValue, 10) : null;
+  });
 
-  useEffect(() => {
-    if (isAuthenticated && token) {
-      localStorage.setItem("TOKEN_KEY", token);
-    }
-  }, [token, isAuthenticated]);
-  useEffect(() => {
-    if (!isAuthenticated) {
-      localStorage.removeItem("TOKEN_KEY");
-      localStorage.removeItem("EXPIRATION_TIME");
-    }
-  }, [isAuthenticated]);
-
-  const loginCtx = (newToken: string, expiration: number) => {
-    console.log(newToken, expiration);
-    setToken(newToken);
-    setIsAuthenticated(true);
-    localStorage.setItem("TOKEN_KEY", newToken);
-    localStorage.setItem("EXPIRATION_TIME", expiration.toString());
-  };
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!token && !isTokenExpired(expirationTime),
+  );
 
   const logout = () => {
     setToken(null);
     setIsAuthenticated(false);
     localStorage.removeItem("TOKEN_KEY");
     localStorage.removeItem("EXPIRATION_TIME");
+  };
+
+  useEffect(() => {
+    if (!token || isTokenExpired(expirationTime)) {
+      logout();
+    } else {
+      setIsAuthenticated(true);
+    }
+  }, [token, expirationTime]);
+
+  useEffect(() => {
+    if (token) {
+      localStorage.setItem("TOKEN_KEY", token);
+    }
+  }, [token]);
+
+  const loginCtx = (newToken: string, expiration: number) => {
+    setToken(newToken);
+    setExpirationTime(expiration);
+    setIsAuthenticated(true);
+    localStorage.setItem("TOKEN_KEY", newToken);
+    localStorage.setItem("EXPIRATION_TIME", expiration.toString());
   };
 
   return (
