aa4d1baad946e5f327dc6f27d155724b5e014ac2
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-08-08 19:23:04 -0400

    refactor: change the card style of special package slider

--- src/components/home/Card.tsx
+++ src/components/home/Card.tsx
@@ -1,4 +1,3 @@
-import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
 type Props = {
@@ -9,36 +8,53 @@ type Props = {
   };
 };
 
-const childVariant = {
-  hidden: { opacity: 0, scale: 0.9 },
-  visible: { opacity: 1, scale: 1 },
-};
-
 const Card = ({ slide }: Props) => {
   return (
-    <motion.div
-      variants={childVariant}
-      className="w-fit cursor-pointer p-4 text-slate-50 transition-all duration-500 hover:scale-110"
-    >
-      <img
-        className="h-[14rem] w-full bg-cover object-cover md:h-[22rem]"
-        src={slide.img}
-        alt="image"
-      />
-      <div className="flex flex-col gap-2 py-2">
-        <div className="flex justify-between rounded-sm bg-golden-800 p-[0.15rem] px-4 py-2">
-          <span>Special Offers</span>
-          <Link
-            className="transition duration-500 hover:text-black"
-            to="/booking"
-          >
-            Book Now
-          </Link>
-        </div>
-        <h1>{slide.title}</h1>
-        <p>{slide.description}</p>
+    <div className="card group relative z-[1] flex h-[400px] w-[280px] flex-col justify-end gap-[0.5em] overflow-hidden rounded-[1.5em] p-[1.5em]">
+      <div className="absolute left-0 top-0 h-full w-full overflow-hidden rounded-2xl opacity-70">
+        <img
+          className="h-full w-full object-cover transition duration-300 group-hover:blur-sm"
+          src={slide.img}
+          alt="image"
+        />
       </div>
-    </motion.div>
+
+      <div className="container relative z-[2] flex flex-col gap-[0.5em] text-black">
+        <div className="h-fit w-full">
+          <p className="text-2xl font-bold text-white">Room Name</p>
+
+          <p className="text-md font-bold text-white">Discount</p>
+        </div>
+
+        <div className="flex h-fit w-fit items-center justify-center gap-[0.5em]">
+          {["Luxury", "Relax", "Family"].map((tag) => (
+            <div
+              key={tag}
+              className="font-nunito cursor-pointer rounded bg-golden-800 px-[0.5em] py-[0.05em] text-[1em] font-normal text-white duration-300 hover:bg-white hover:text-[#222222]"
+            >
+              <p>{tag}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div>
+        <p className="font-nunito relative h-[0em] overflow-hidden font-light leading-[1.2em] text-white duration-500 group-hover:h-[7em]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis,
+          nesciunt sed. Voluptatibus sunt doloremque dolorum facere dignissimos
+          rerum hic vitae architecto ut, nihil quas velit eveniet aliquid totam
+          voluptate nulla.
+          <br />
+          <span className="absolute bottom-2">
+            <Link
+              className="z-50 rounded bg-golden-800 p-2 font-semibold text-white transition duration-500 hover:text-black"
+              to="/booking"
+            >
+              See Details
+            </Link>
+          </span>
+        </p>
+      </div>
+    </div>
   );
 };
 
--- src/components/home/SlickCarousel.tsx
+++ src/components/home/SlickCarousel.tsx
@@ -33,49 +33,11 @@ const SlickCarousel = () => {
   const settings = {
     autoplay: true,
     autoplaySpeed: 5000,
-    // pauseOnFocus: false,
-    // draggable: true,
-    // infinite: true,
-    // dots: false,
-    // arrows: true,
+
     speed: 1000,
     mobileFirst: true,
     slidesToShow: 3,
     slidesToScroll: 1,
-    // responsive: [
-    //   {
-    //     breakpoint: 576,
-    //     settings: {
-    //       slidesToShow: 4,
-    //       slidesToScroll: 4,
-    //       arrows: true,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 768,
-    //     settings: {
-    //       slidesToShow: 5,
-    //       slidesToScroll: 5,
-    //       arrows: true,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 992,
-    //     settings: {
-    //       slidesToShow: 6,
-    //       slidesToScroll: 6,
-    //       arrows: true,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 1200,
-    //     settings: {
-    //       slidesToShow: 7,
-    //       slidesToScroll: 7,
-    //       arrows: true,
-    //     },
-    //   },
-    // ],
   };
 
   return (
